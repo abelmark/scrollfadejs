3e8b460bb5e4356312992d10f7eae529cd3ff9cb
abelmark <mabel16113@gmail.com>
2018-01-09 22:39:30 -0500

    removed unused code

--- index.js
+++ index.js
@@ -28,13 +28,8 @@ ColorFadeJS = {
   },
 
   scrollListener: function() {
-    console.log('scroll listener has fired');
-    
     window.addEventListener("scroll", () =>{
-      // Array.prototype.forEach.call(s.containers, (container)=>{
       Array.from(document.getElementsByClassName(s.containerName)).forEach((container, index) => {
-        console.log('container', container)
-        console.log('index', index)
         if(container.offsetTop < window.pageYOffset + 100 && container.offsetTop > window.pageYOffset - 100){
           console.log(true)
           return document.querySelector('body').style.background = s.colors[index];
